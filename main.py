4317d333d004adf4594db62b5bc55e541fbb468a
Mohamed Sabry <44135662+IMSabry@users.noreply.github.com>
2020-09-27 15:28:22 +0200

    Edit

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 import pyttsx3
 import PyPDF2
 
-book = open('oop.pdf', 'rb')  # open pdf location
-pdfReader = PyPDF2.PdfFileReader(book)
-pages = pdfReader.numPages
-print(pages)
+book = open('<name.pdf>', 'rb')  # open pdf location
+pdfReader = PyPDF2.PdfFileReader(book)  # access the book
+pages = pdfReader.numPages  # access the number of pages
+# print(pages)
 
 speaker = pyttsx3.init()
 
-for num in range(7, pages):  # from start page to end page
+for num in range(7, pages):  # from page number 7 to end page
     page = pdfReader.getPage(7)
     text = page.extractText()
     speaker.say(text)
